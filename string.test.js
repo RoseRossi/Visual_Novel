1312b34adba43b224f8ba40f1f32fcb951ee6d4d
Isabela <isabela.rosero@correounivalle.edu.co>
2023-10-22 11:01:18 -0500

    String change

--- string.test.js
+++ string.test.js
@@ -1,6 +1,6 @@
 const checkString = require('./checkString.js');
 
 test('checks if a string is not empty', () => {
-  expect(checkString('Hello')).toBe(true);
+  expect(checkString('Good')).toBe(true);
   expect(checkString('')).toBe(false);
 });
