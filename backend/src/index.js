850801adebafaa79c5139aa133056e9e4ef24b39
Cristian Machado <cmachado@aventi.com>
2023-10-23 13:32:45 -0500

    New Struture and docker configuration

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+//libs
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+
+//inicializaciones
+const app = express();
+
+//Puerto para el despliegue en producción
+const { PORT } = process.env;
+
+//configuraciones
+app.set('port', PORT || 3700);
+
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+
+//despliegue del servidor
+app.listen(app.get('port'), () =>
+{
+    console.log('Server on port', app.get('port'));
+});
