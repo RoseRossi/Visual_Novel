7e484b019819b8e7f4239c439afa00e412d865ce
Cristian Duvan Machado <cristian.machado@correounivalle.edu.co>
2023-11-01 22:43:44 -0500

    Finish Deploymend

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,8 +6,14 @@ const {
 
 const router = Router();
 
-router.post('/isUserRegistered', isUserRegistered);
-router.post('/registerUser', registerPersonalData);
+router.post('/backend/isUserRegistered', isUserRegistered);
+router.post('/backend/registerUser', registerPersonalData);
+router.get('/', (req, res) => {
+    res.send('This is the backend of the app');
+});
+router.get('/backend', (req, res) => {
+    res.send('This is the backend of the app');
+});
 
 // Export the router
 module.exports = router;
