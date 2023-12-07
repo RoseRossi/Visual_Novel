323f2a745f18c01343b375bd889f188acd5cff17
unknown <javier.posso@correounivalle.edu.co>
2023-12-07 16:12:15 -0500

    Cambio de modelos para optimizacion

--- frontend/src/components/pages/Scene3/Parts/Bag.jsx
+++ frontend/src/components/pages/Scene3/Parts/Bag.jsx
@@ -1,18 +1,19 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export function Bag(props) {
-    const { nodes, materials } = useGLTF("/assets/models/bag.glb");
-    return (
-        <group {...props} dispose={null}>
-            <mesh
-                castShadow
-                receiveShadow
-                geometry={nodes.mesh_0.geometry}
-                material={nodes.mesh_0.material}
-            />
-        </group>
-    );
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/assets/models/bag.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Bag_Bag_0.geometry}
+        material={materials.material}
+        scale={1.404}
+      />
+    </group>
+  );
 }
 
 export default Bag;
